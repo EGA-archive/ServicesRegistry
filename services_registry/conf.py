c1acca382efe206457fb40e2f71a14b4ff5acbda
Sabela <sabela.delatorre@crg.eu>
2020-06-12 17:57:03 +0200

    Fix regex

--- services_registry/conf.py
+++ services_registry/conf.py
@@ -8,12 +8,11 @@ services = [ ('EGA Beacon v0.3', 'https://ega-archive.org/beacon-api/'),
 			#('EGA Beacon', 'https://beacon-api.ega-archive.org'),
 			 ('Covid Beacon', 'https://covid19beacon.crg.eu/api/')]
 
-# either whitelist or blacklist should be provided
+
 urls_whitelisted = ['^\/$',
 					'^\/service-info$',
-					'^\/query\?[\/a-zA-Z0-9]*',
-					'^\/g_variants[\/a-zA-Z0-9]*',
-					'^\/biosamples[\/a-zA-Z0-9]*',]
+					'^\/query\?.*',
+					'^\/g_variants.*',
+					'^\/biosamples.*',]
 
 urls_blacklisted = []
-# urls_blacklisted = ['^\/individuals[\/a-zA-Z0-9]*']
