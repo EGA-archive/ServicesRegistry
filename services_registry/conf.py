a89a24b8a4158904dbf73b7c8cf5434b21935206
Sabela <sabela.delatorre@crg.eu>
2020-06-12 12:19:11 +0200

    Make it work w/o any validation. Add logging config

--- services_registry/conf.py
+++ services_registry/conf.py
@@ -4,8 +4,11 @@ port = 8000
 # import ssl
 ssl_context = None
 
-services = ['https://beacon-api.ega-archive.org',
-	    'https://covid19beacon.crg.eu']
+services = [ ('EGA Beacon v0.3', 'https://ega-archive.org/beacon-api/'),
+			#('EGA Beacon', 'https://beacon-api.ega-archive.org'),
+			 ('Covid Beacon', 'https://covid19beacon.crg.eu/api/')]
 
 # services = ['https://service-1.domain.tld',
 # 	      'https://service-2.domain.tld']
+
+urls = []
--- services_registry/dispatcher.py
+++ services_registry/dispatcher.py
@@ -12,44 +12,44 @@ import httpx
 from aiohttp import web
 
 from . import conf
-# from .validator import checker
+from .validator import checker
 
 LOG = logging.getLogger(__name__)
 LOG_FILE = Path(os.getenv('SERVICES_REGISTRY_LOG', 'logger.yml')).resolve()
 
 
-# SERVICES = []
+SERVICES = conf.services
 # URLS = checker()
 
-# async def response_from_services(request):
+async def response_from_services(request):
+    LOG.info('-------- response_from_services %s', request.path_qs)
 
-#     # Allow only GET and POST ?
-
-#     for name, address in SERVICES:
-#         async with httpx.AsyncClient() as client:
-#             url = f'{address}{request.path_qs}'
-#             LOG.info('%s %s', request.method, url)
-#             r = await client.request(request.method,
-#                                      url,
-#                                      headers=request.headers,
-#                                      data=None if request.method == 'GET' else request.post())
-#             if r.status_code > 200:
-#                 LOG.error("Invalid response [%s] for %s", r.status_code, name)
-#                 response = f"Invalid response {r.status_code}" 
-#             else:
-#                 response = await r.json()
+    # Allow only GET and POST ?
+    for name, address in SERVICES:
+        url = f'{address}{request.path_qs}'
+        LOG.info('%s %s', request.method, url)
+        async with httpx.AsyncClient() as client:
+            r = await client.request(request.method,
+                                     url,
+                                     # headers=request.headers,
+                                     data=None if request.method == 'GET' else request.post())
+            if r.status_code > 200:
+                LOG.error("Invalid response [%s] for %s", r.status_code, name)
+                response = f"Invalid response {r.status_code}"
+            else:
+                response = r.json()
                         
-#             yield (name, { 'request': url , 'response': response })
+            yield (name, { 'request': url , 'response': response })
                 
 
-# async def forward_endpoint(request):
-#     if not URLS.validate(request.path_qs):
-#         LOG.error('Invalid Query %s', request.path_qs)
-#         raise web.HTTPForbidden(reason='Invalid request from whitelist/blacklist')
+async def forward_endpoint(request):
+    # if not URLS.validate(request.path_qs):
+    #     LOG.error('Invalid Query %s', request.path_qs)
+    #     raise web.HTTPForbidden(reason='Invalid request from whitelist/blacklist')
 
-#     LOG.info('-------- Aggregator query %s', request.path_qs)
-#     response = response_from_services(request)
-#     return web.json_response( dict([r async for r in response]) ) # change that for streaming response
+    LOG.info('-------- Aggregator query %s', request.path_qs)
+    response = response_from_services(request)
+    return web.json_response( dict([r async for r in response]) ) # change that for streaming response
 
 async def dead(request):
     LOG.debug('deadend %s', request.path_qs)
@@ -58,7 +58,7 @@ async def dead(request):
 def main(path=None):
 
     # Configure basic logging
-    logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL, format='[%(levelname)s] %(message)s')
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='[%(levelname)s] %(message)s')
     # Configure more logging, if found
     if LOG_FILE.exists():
         with open(LOG_FILE, 'r') as stream:
@@ -69,7 +69,7 @@ def main(path=None):
 
     # Add the routes
     server.add_routes([web.get('/favicon.ico', dead),
-                       #web.get('/{anything:.*}', forward_endpoint)
+                       web.get('/{anything:.*}', forward_endpoint)
     ])
 
     # .... and cue music!
